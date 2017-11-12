77347571f1578f231945e6a34d4713f3858c9cd7
xschen <xs0040@gmail.com>
2017-11-12 10:15:32 +0800

    data exploration

--- qa_system_train/squad_explore_data.py
+++ qa_system_train/squad_explore_data.py
@@ -1,6 +1,12 @@
 import json
+import nltk
 
 DATA_PATH = 'data/SQuAD/train-v1.1.json'
+data = []
+
+max_context_length = 0
+max_question_length = 0
+max_answer_length = 0
 
 with open(DATA_PATH) as file:
     json_data = json.load(file)
@@ -8,11 +14,18 @@ with open(DATA_PATH) as file:
     for instance in json_data['data']:
         for paragraph in instance['paragraphs']:
             context = paragraph['context']
-            print('context: ', context)
+            max_context_length = max(max_context_length, len([w for w in nltk.word_tokenize(context)]))
             qas = paragraph['qas']
             for qas_instance in qas:
                 question = qas_instance['question']
-                print('question: ', question)
                 answers = qas_instance['answers']
+                max_question_length = max(max_question_length, len([w for w in nltk.word_tokenize(question)]))
                 for answer in answers:
-                    print('answer: ', answer['text'])
+                    max_answer_length = max(max_answer_length, len([w for w in nltk.word_tokenize(answer['text'])]))
+                    data.append((context, question, answer['text']))
+
+print('max context length: ', max_context_length)
+print('max question length: ', max_question_length)
+print('max answer length: ', max_answer_length)
+
+print('data count: ', len(data))
