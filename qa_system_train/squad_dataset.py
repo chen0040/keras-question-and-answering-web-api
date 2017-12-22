a4b71b648c2b139bdf52542953d9b18c0197d4e3
xschen <xs0040@gmail.com>
2017-12-22 11:14:08 +0800

    fix seq2seq glove v2 predict model

--- qa_system_train/squad_dataset.py
+++ qa_system_train/squad_dataset.py
@@ -183,6 +183,151 @@ class SQuADSeq2SeqTupleSamples(object):
                                 random_state=random_state)
 
 
+class SQuADSeq2SeqTripleSamples(object):
+    input_paragraph_max_seq_length = None
+    input_question_max_seq_length = None
+    target_max_seq_length = None
+
+    input_paragraph_word2idx = None
+    input_question_word2idx = None
+    target_word2idx = None
+
+    input_paragraph_idx2word = None
+    input_question_idx2word = None
+    target_idx2word = None
+
+    num_input_paragraph_tokens = None
+    num_input_question_tokens = None
+    num_target_tokens = None
+
+    samples = None
+
+    dataset = None
+
+    def __init__(self, dataset):
+        self.dataset = dataset
+        self.input_data_samples = []
+        self.output_data_samples = []
+
+        self.input_paragraph_max_seq_length = 0
+        self.input_question_max_seq_length = 0
+        self.target_max_seq_length = 0
+
+        input_paragraph_counter = Counter()
+        input_question_counter = Counter()
+        target_counter = Counter()
+
+        input_data_samples = []
+        output_data_samples = []
+
+        for sample in self.dataset.data:
+            paragraph, question, answer = sample
+            paragraph_word_list = [w.lower() for w in nltk.word_tokenize(paragraph) if in_white_list(w)]
+            question_word_list = [w.lower() for w in nltk.word_tokenize(question) if in_white_list(w)]
+            answer_word_list = [w.lower() for w in nltk.word_tokenize(answer) if in_white_list(w)]
+
+            output_data = ['START'] + answer_word_list + ['END']
+
+            input_data_samples.append([paragraph_word_list, question_word_list])
+            output_data_samples.append(output_data)
+
+            for w in paragraph_word_list:
+                input_paragraph_counter[w] += 1
+            for w in question_word_list:
+                input_question_counter[w] += 1
+            for w in output_data:
+                target_counter[w] += 1
+
+            self.input_paragraph_max_seq_length = max(self.input_paragraph_max_seq_length, len(paragraph_word_list))
+            self.input_question_max_seq_length = max(self.input_question_max_seq_length, len(question_word_list))
+            self.target_max_seq_length = max(self.target_max_seq_length, len(output_data))
+
+        self.input_paragraph_word2idx = dict()
+        self.input_question_word2idx = dict()
+        self.target_word2idx = dict()
+        for idx, word in enumerate(input_paragraph_counter.most_common(MAX_INPUT_VOCAB_SIZE)):
+            self.input_paragraph_word2idx[word[0]] = idx + 2
+        for idx, word in enumerate(input_question_counter.most_common(MAX_INPUT_VOCAB_SIZE)):
+            self.input_question_word2idx[word[0]] = idx + 2
+        for idx, word in enumerate(target_counter.most_common(MAX_TARGET_VOCAB_SIZE)):
+            self.target_word2idx[word[0]] = idx + 1
+
+        self.target_word2idx['UNK'] = 0
+        self.input_paragraph_word2idx['PAD'] = 0
+        self.input_paragraph_word2idx['UNK'] = 1
+        self.input_question_word2idx['PAD'] = 0
+        self.input_paragraph_word2idx['UNK'] = 1
+
+        self.input_paragraph_idx2word = dict([(idx, word) for word, idx in self.input_paragraph_word2idx.items()])
+        self.input_question_idx2word = dict([(idx, word) for word, idx in self.input_question_word2idx.items()])
+        self.target_idx2word = dict([(idx, word) for word, idx in self.target_word2idx.items()])
+
+        self.num_input_paragraph_tokens = len(self.input_paragraph_idx2word)
+        self.num_input_question_tokens = len(self.input_question_idx2word)
+        self.num_target_tokens = len(self.target_idx2word)
+
+        input_encoded_data_samples = []
+        target_encoded_data_samples = []
+
+        for input_data, output_data in zip(input_data_samples, output_data_samples):
+            input_paragraph_encoded_data = []
+            input_question_encoded_data = []
+            target_encoded_data = []
+            input_paragraph_data, input_question_data = input_data
+            for word in input_paragraph_data:
+                if word in self.input_paragraph_word2idx:
+                    input_paragraph_encoded_data.append(self.input_paragraph_word2idx[word])
+                else:
+                    input_paragraph_encoded_data.append(1)
+            for word in input_question_data:
+                if word in self.input_question_word2idx:
+                    input_question_encoded_data.append(self.input_question_word2idx[word])
+                else:
+                    input_question_encoded_data.append(1)
+            for word in output_data:
+                if word in self.target_word2idx:
+                    target_encoded_data.append(self.target_word2idx[word])
+                else:
+                    target_encoded_data.append(0)
+            input_encoded_data_samples.append([input_paragraph_encoded_data, input_question_encoded_data])
+            target_encoded_data_samples.append(target_encoded_data)
+
+        self.samples = [input_encoded_data_samples, target_encoded_data_samples]
+
+    def save(self, dir_path, tag=None):
+        name = 'seq2seq'
+        if tag is not None:
+            name = name + '-' + tag
+        np.save(dir_path + '/' + name + '-input-paragraph-word2idx.npy', self.input_paragraph_word2idx)
+        np.save(dir_path + '/' + name + '-input-paragraph-idx2word.npy', self.input_paragraph_idx2word)
+        np.save(dir_path + '/' + name + '-input-question-word2idx.npy', self.input_question_word2idx)
+        np.save(dir_path + '/' + name + '-input-question-idx2word.npy', self.input_question_idx2word)
+        np.save(dir_path + '/' + name + '-target-word2idx.npy', self.target_word2idx)
+        np.save(dir_path + '/' + name + '-target-idx2word.npy', self.target_idx2word)
+
+        config = dict()
+        config['num_input_question_tokens'] = self.num_input_question_tokens
+        config['num_input_paragraph_tokens'] = self.num_input_paragraph_tokens
+        config['num_target_tokens'] = self.num_target_tokens
+        config['input_paragraph_max_seq_length'] = self.input_paragraph_max_seq_length
+        config['input_question_max_seq_length'] = self.input_question_max_seq_length
+        config['target_max_seq_length'] = self.target_max_seq_length
+
+        print(config)
+        np.save(dir_path + '/' + name + '-config.npy', config)
+
+    def size(self):
+        return self.dataset.size()
+
+    def get_samples(self):
+        return self.samples
+
+    def split(self, test_size, random_state):
+        input_data, target_data = self.samples
+        return train_test_split(input_data, target_data, test_size=test_size,
+                                random_state=random_state)
+
+
 class SQuADSeq2SeqEmbTupleSamples(object):
     input_max_seq_length = None
     target_max_seq_length = None
--- qa_system_train/squad_seq2seq_v2_train.py
+++ qa_system_train/squad_seq2seq_v2_train.py
@@ -3,20 +3,21 @@ from keras.layers.recurrent import LSTM
 from keras.layers import Dense, Input, Embedding
 from keras.preprocessing.sequence import pad_sequences
 from keras.callbacks import ModelCheckpoint
-from qa_system_train.squad_dataset import SquADDataSet, SQuADSeq2SeqTupleSamples
+from qa_system_train.squad_dataset import SquADDataSet, SQuADSeq2SeqTripleSamples
 import numpy as np
 
 np.random.seed(42)
 
 BATCH_SIZE = 64
 NUM_EPOCHS = 100
-MAX_VOCAB_SIZE = 600
+HIDDEN_UNITS = 256
 MODEL_DIR_PATH = 'models/SQuAD'
-WEIGHT_FILE_PATH = MODEL_DIR_PATH + '/seq2seq-weights.h5'
+WEIGHT_FILE_PATH = MODEL_DIR_PATH + '/seq2seq-v2-weights.h5'
+ARCHITECTURE_FILE_PATH = MODEL_DIR_PATH + '/seq2seq-v2-architecture.json'
 
 
 dataset = SquADDataSet(10000)
-dataset_seq2seq = SQuADSeq2SeqTupleSamples(dataset)
+dataset_seq2seq = SQuADSeq2SeqTripleSamples(dataset)
 dataset_seq2seq.save(MODEL_DIR_PATH)
 
 
@@ -42,11 +43,22 @@ def generate_batch(ds, input_data, target_data):
             yield [encoder_input_data_batch, decoder_input_data_batch], decoder_target_data_batch
 
 
-encoder_inputs = Input(shape=(None,), name='encoder_inputs')
-encoder_embedding = Embedding(input_dim=dataset_seq2seq.num_input_tokens, output_dim=HIDDEN_UNITS,
-                              input_length=dataset_seq2seq.input_max_seq_length, name='encoder_embedding')
+context_inputs = Input(shape=(None,), name='context_inputs')
+encoded_context = Embedding(input_dim=num_context_tokens, output_dim=glove.EMBED_HIDDEN_UNITS,
+                            input_length=context_max_seq_length, name='context_embedding')(context_inputs)
+encoded_context = Dropout(0.3)(encoded_context)
+
+question_inputs = Input(shape=(None,), name='question_inputs')
+encoded_question = Embedding(input_dim=num_question_tokens, output_dim=glove.EMBED_HIDDEN_UNITS,
+                             input_length=question_max_seq_length, name='question_embedding')(question_inputs)
+encoded_question = Dropout(0.3)(encoded_question)
+encoded_question = LSTM(units=glove.EMBED_HIDDEN_UNITS, name='question_lstm')(encoded_question)
+encoded_question = RepeatVector(context_max_seq_length)(encoded_question)
+
+merged = add([encoded_context, encoded_question])
+
 encoder_lstm = LSTM(units=HIDDEN_UNITS, return_state=True, name='encoder_lstm')
-encoder_outputs, encoder_state_h, encoder_state_c = encoder_lstm(encoder_embedding(encoder_inputs))
+encoder_outputs, encoder_state_h, encoder_state_c = encoder_lstm(merged)
 encoder_states = [encoder_state_h, encoder_state_c]
 
 decoder_inputs = Input(shape=(None, dataset_seq2seq.num_target_tokens), name='decoder_inputs')
@@ -61,7 +73,7 @@ model = Model([encoder_inputs, decoder_inputs], decoder_outputs)
 model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
 
 json = model.to_json()
-open(MODEL_DIR_PATH + '/seq2seq-architecture.json', 'w').write(json)
+open(ARCHITECTURE_FILE_PATH, 'w').write(json)
 
 Xtrain, Xtest, Ytrain, Ytest = dataset_seq2seq.split(test_size=0.2, random_state=42)
 
