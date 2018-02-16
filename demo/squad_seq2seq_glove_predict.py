3e593419eb015ae11dd089d1cd4e2a37f724d388
xschen <xs0040@gmail.com>
2018-02-16 23:08:49 +0800

    predict scripts for glove and v2 seq2seq

--- demo/squad_seq2seq_glove_predict.py
+++ demo/squad_seq2seq_glove_predict.py
@@ -1,9 +1,10 @@
-from keras_question_and_answering_system.library.seq2seq import Seq2SeqQA
+from keras_question_and_answering_system.library.seq2seq_glove import Seq2SeqGloveQA
 from keras_question_and_answering_system.library.utility.squad import SquADDataSet
 
 
 def main():
-    qa = Seq2SeqQA()
+    qa = Seq2SeqGloveQA()
+    qa.load_glove_model('./very_large_data')
     qa.load_model(model_dir_path='./models')
     data_set = SquADDataSet(data_path='./data/SQuAD/train-v1.1.json')
     for i in range(20):
--- demo/squad_seq2seq_v2_predict.py
+++ demo/squad_seq2seq_v2_predict.py
@@ -1,9 +1,9 @@
-from keras_question_and_answering_system.library.seq2seq import Seq2SeqQA
+from keras_question_and_answering_system.library.seq2seq_v2 import Seq2SeqV2QA
 from keras_question_and_answering_system.library.utility.squad import SquADDataSet
 
 
 def main():
-    qa = Seq2SeqQA()
+    qa = Seq2SeqV2QA()
     qa.load_model(model_dir_path='./models')
     data_set = SquADDataSet(data_path='./data/SQuAD/train-v1.1.json')
     for i in range(20):
