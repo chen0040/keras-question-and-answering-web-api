8b2c4e515acb606042d3dafc78e1a14c594ea711
xschen <xs0040@gmail.com>
2018-02-16 23:21:57 +0800

    complete the second implementation glove v2 refactored code

--- keras_question_and_answering_system/library/seq2seq_v2_glove.py
+++ keras_question_and_answering_system/library/seq2seq_v2_glove.py
@@ -1,15 +1,43 @@
 import nltk
 import numpy as np
+from keras.callbacks import ModelCheckpoint
 from keras.layers import Input, LSTM, Dense, Dropout, add, RepeatVector
 from keras.models import Model
 from keras.preprocessing.sequence import pad_sequences
-
 from keras_question_and_answering_system.library.utility.glove_model import GloveModel
+from keras_question_and_answering_system.library.utility.qa_embed_data_utils import SQuADSeq2SeqEmbTripleSamples
 from qa_system_web.text_utils import in_white_list
 import qa_system_web.glove_loader as glove_loader
-
-from qa_system_web.squad_dataset import SquADDataSet
 import os
+
+
+def generate_batch(ds, input_word2em_data, output_data, batch_size):
+    num_batches = len(input_word2em_data) // batch_size
+    while True:
+        for batchIdx in range(0, num_batches):
+            start = batchIdx * batch_size
+            end = (batchIdx + 1) * batch_size
+            encoder_input_paragraph_data_batch = []
+            encoder_input_question_data_batch = []
+            for input_paragraph_data, input_question_data in input_word2em_data[start:end]:
+                encoder_input_paragraph_data_batch.append(input_paragraph_data)
+                encoder_input_question_data_batch.append(input_question_data)
+            encoder_input_paragraph_data_batch = pad_sequences(encoder_input_paragraph_data_batch,
+                                                               ds.input_paragraph_max_seq_length)
+            encoder_input_question_data_batch = pad_sequences(encoder_input_question_data_batch,
+                                                              ds.input_question_max_seq_length)
+            decoder_target_data_batch = np.zeros(shape=(batch_size, ds.target_max_seq_length, ds.num_target_tokens))
+            decoder_input_data_batch = np.zeros(shape=(batch_size, ds.target_max_seq_length, ds.num_target_tokens))
+            for lineIdx, target_wid_list in enumerate(output_data[start:end]):
+                for idx, wid in enumerate(target_wid_list):
+                    if wid == 0:  # UNKNOWN
+                        continue
+                    decoder_input_data_batch[lineIdx, idx, wid] = 1
+                    if idx > 0:
+                        decoder_target_data_batch[lineIdx, idx - 1, wid] = 1
+            yield [encoder_input_paragraph_data_batch, encoder_input_question_data_batch,
+                   decoder_input_data_batch], decoder_target_data_batch
+
 
 class Seq2SeqGloveV2QA(object):
     model_name = 'seq2seq-qa-glove-v2'
@@ -87,6 +115,55 @@ class Seq2SeqGloveV2QA(object):
         decoder_outputs = decoder_dense(decoder_outputs)
         self.decoder_model = Model([decoder_inputs] + decoder_state_inputs, [decoder_outputs] + decoder_states)
 
+    def fit(self, data_set, model_dir_path, epochs=None, batch_size=None, test_size=None, random_state=None):
+        if batch_size is None:
+            batch_size = 64
+        if epochs is None:
+            epochs = 100
+        if test_size is None:
+            test_size = 0.2
+        if random_state is None:
+            random_state = 42
+
+        data_set_seq2seq = SQuADSeq2SeqEmbTripleSamples(data_set, self.glove_model.word2em,
+                                                        self.glove_model.embedding_size)
+        data_set_seq2seq.save(model_dir_path, 'qa-glove-v2')
+
+        x_train, x_test, y_train, y_test = data_set_seq2seq.split(test_size=test_size, random_state=random_state)
+
+        print(len(x_train))
+        print(len(x_test))
+
+        self.max_encoder_paragraph_seq_length = data_set_seq2seq.input_paragraph_max_seq_length
+        self.max_encoder_question_seq_length = data_set_seq2seq.input_question_max_seq_length
+        self.max_decoder_seq_length = data_set_seq2seq.target_max_seq_length
+        self.num_decoder_tokens = data_set_seq2seq.num_target_tokens
+
+        weight_file_path = self.get_weight_file_path(model_dir_path)
+        architecture_file_path = self.get_architecture_file_path(model_dir_path)
+
+        self.create_model()
+
+        with open(architecture_file_path, 'w') as f:
+            f.write(self.model.to_json())
+
+        train_gen = generate_batch(data_set_seq2seq, x_train, y_train, batch_size)
+        test_gen = generate_batch(data_set_seq2seq, x_test, y_test, batch_size)
+
+        train_num_batches = len(x_train) // batch_size
+        test_num_batches = len(x_test) // batch_size
+
+        checkpoint = ModelCheckpoint(filepath=weight_file_path, save_best_only=True)
+
+        history = self.model.fit_generator(generator=train_gen, steps_per_epoch=train_num_batches,
+                                           epochs=epochs,
+                                           verbose=1, validation_data=test_gen, validation_steps=test_num_batches,
+                                           callbacks=[checkpoint])
+
+        self.model.save_weights(weight_file_path)
+
+        return history
+
     def reply(self, paragraph, question):
         input_paragraph_seq = []
         input_question_seq = []
@@ -133,9 +210,10 @@ class Seq2SeqGloveV2QA(object):
             states_value = [h, c]
         return target_text.strip()
 
-    def test_run(self, ds, index):
+    def test_run(self, ds, index=None):
+        if index is None:
+            index = 0
         paragraph, question, actual_answer = ds.get_data(index)
         predicted_answer = self.reply(paragraph, question)
         # print({'context': paragraph, 'question': question})
         print({'predict': predicted_answer, 'actual': actual_answer})
-
