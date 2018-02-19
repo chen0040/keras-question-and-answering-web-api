78ca458311931ec9684df3cf0a34b9468743dd40
xschen <xs0040@gmail.com>
2018-02-19 15:25:55 +0800

    update doc

--- demo/squad_explore.py
+++ demo/squad_explore.py
@@ -4,6 +4,12 @@ from keras_question_and_answering_system.library.utility.squad import SquADDataS
 def main():
     data_set = SquADDataSet(data_path='./data/SQuAD/train-v1.1.json')
     print('size: ', data_set.size())
+    for index in range(20):
+        context, question, answer = data_set.get_data(index)
+        print('#' + str(index) + ' context:', context)
+        print('#' + str(index) + ' question:', question)
+        print('#' + str(index) + ' answer:', answer)
+        print('++++++++++++++++++++++++++++++++++++++++++++++++++++')
 
 
 if __name__ == '__main__':
