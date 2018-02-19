ff1c9472c53c534f684cdd288307b6199551c19f
xschen <xs0040@gmail.com>
2018-02-19 15:20:08 +0800

    save the trained model of seq2seq v2 at epochs 200

---
+++ demo/square_compare_models.py
@@ -0,0 +1,17 @@
+import os
+import numpy as np
+
+
+def main():
+    model_dir_path = './models'
+    models = ['seq2seq', 'seq2seq-v2', 'seq2seq-glove']
+    histories = list()
+    for model_name in models:
+        history_file_name = model_name + '-history.npy'
+        history_file_path = os.path.join(model_dir_path, history_file_name)
+        history = np.load(history_file_path).item()
+        print(history_file_name, history)
+
+
+if __name__ == '__main__':
+    main()
